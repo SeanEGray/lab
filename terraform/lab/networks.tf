c9d35db2c4090d1d3a955399fe81c1d521014de5
SeanEGray <sean.e.gray@outlook.com>
2018-05-05 14:02:39 +0100

    Add networks.tf

---
+++ terraform/lab/networks.tf
@@ -0,0 +1,29 @@
+resource "azurerm_resource_group" "lab_rg" {
+    name = "${var.resource_group_name}"
+    location = "${var.location}"
+}
+
+resource "azurerm_virtual_network" "lab_vnet" {
+    name = "lab vnet"
+    address_space = ["${var.vnet_cidr}"]
+    location = "${var.location}"
+    resource_group_name = "${azurerm_resource_group.lab_rg.name}"
+
+    tags {
+        group = "Terraform lab"
+    }
+}
+
+resource "azurerm_subnet" "lab_subnet_1" {
+    name = "lab subnet 1"
+    address_prefix = "${var.subnet1_cidr}"
+    virtual_network_name = "${azurerm_virtual_network.lab_vnet.name}"
+    resource_group_name = "${azurerm_resource_group.lab_rg.name}"
+}
+
+resource "azurerm_subnet" "lab_subnet_2" {
+    name = "lab subnet 2"
+    address_prefix = "${var.subnet2_cidr}"
+    virtual_network_name = "${azurerm_virtual_network.lab_vnet.name}"
+    resource_group_name = "${azurerm_resource_group.lab_rg.name}"
+}
