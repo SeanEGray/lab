3027b405ce6c1e5dfffcca2a31220064d73f8c6d
SeanEGray <sean.e.gray@outlook.com>
2018-06-24 19:47:52 +0100

    Add web NSG

--- terraform/lab/networks.tf
+++ terraform/lab/networks.tf
@@ -84,3 +84,34 @@ resource "azurerm_network_security_group" "adds_nsg" {
     destination_address_prefix = "*"
   }
 }
+
+
+resource "azurerm_network_security_group" "web_nsg" {
+  name                = "web-NSG"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.lab_rg.name}"
+
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 201
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefixes    = "*"
+    destination_address_prefix = "*"
+  }
+}
