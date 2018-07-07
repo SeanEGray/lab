390ebe27dfafc58dc1a281ce3bee23f48570a834
Sean Gray <sean.e.gray@outlook.com>
2018-07-07 18:47:02 +0100

    Remove superfluous blob upload

--- terraform/lab/storage.tf
+++ terraform/lab/storage.tf
@@ -36,11 +36,4 @@ resource "azurerm_storage_share" "testshare" {
   quota = 1
 }
 
-resource "azurerm_storage_blob" "dsc_metaconfig" {
-  name = ""
-  resource_group_name    = "${azurerm_resource_group.lab_rg.name}"
-  storage_account_name   = "${azurerm_storage_account.lab_storage1.name}"
-  storage_container_name = "${azurerm_storage_container.lab_container.name}"
-  source = "${var.dsc_metaconfig_path}"
-  type = "block"
-}
+
